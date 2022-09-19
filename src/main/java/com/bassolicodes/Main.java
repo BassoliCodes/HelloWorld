becd0c21b2913ccae3b5505931dfc261ccf49f80
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2022-09-19 07:52:35 -0300

    chore: remove sendHelloWorld

--- src/main/java/com/bassolicodes/Main.java
+++ src/main/java/com/bassolicodes/Main.java
@@ -2,12 +2,7 @@ package com.bassolicodes;
 
 public class Main {
     public static void main(String[] args) {
-        sendHelloWorld();
         sendHelloWorldTo("Lucca Bassoli");
-    }
-
-    public static void sendHelloWorld() {
-        System.out.println("Hello world!");
     }
 
     public static void sendHelloWorldTo(String name) {
