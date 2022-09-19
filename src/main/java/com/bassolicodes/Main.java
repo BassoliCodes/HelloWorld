1a16505c195859c7a9d27997e0b879f4db5bf228
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2022-09-19 08:24:39 -0300

    feat: create first http request with okHttp

--- src/main/java/com/bassolicodes/Main.java
+++ src/main/java/com/bassolicodes/Main.java
@@ -1,11 +1,35 @@
 package com.bassolicodes;
 
+import okhttp3.*;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+
+    private final OkHttpClient httpClient = new OkHttpClient();
+    public static void main(String[] args) throws IOException {
         sendHelloWorldTo("Lucca Bassoli");
+
+        Main main = new Main();
+
+        main.sendRequest("https://api.github.com/users/BassoliCodes");
     }
 
     public static void sendHelloWorldTo(String name) {
         System.out.println("Hello " + name + "!");
+    }
+
+    public void sendRequest(String urlRequest) throws IOException {
+        System.out.println("Sending request...");
+
+        Request request = new Request.Builder().url(urlRequest).build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            String body = response.body().string();
+
+            System.out.println(body);
+        }
+
     }
 }
