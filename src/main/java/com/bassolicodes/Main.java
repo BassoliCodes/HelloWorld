f622ebce7005807a9fbb359d5810ceff035faa2d
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2022-09-19 08:37:27 -0300

    feat: create sendrequest class to define pattern

--- src/main/java/com/bassolicodes/Main.java
+++ src/main/java/com/bassolicodes/Main.java
@@ -1,35 +1,22 @@
 package com.bassolicodes;
 
-import okhttp3.*;
+import com.bassolicodes.utils.SendRequest;
+
 import java.io.IOException;
 
 public class Main {
 
-    private final OkHttpClient httpClient = new OkHttpClient();
     public static void main(String[] args) throws IOException {
         sendHelloWorldTo("Lucca Bassoli");
 
-        Main main = new Main();
+        SendRequest sendRequest = new SendRequest();
 
-        main.sendRequest("https://api.github.com/users/BassoliCodes");
+        sendRequest.getUserGitHub("BassoliCodes");
+        sendRequest.getAllContributionsByUser("BassoliCodes");
     }
 
     public static void sendHelloWorldTo(String name) {
         System.out.println("Hello " + name + "!");
     }
 
-    public void sendRequest(String urlRequest) throws IOException {
-        System.out.println("Sending request...");
-
-        Request request = new Request.Builder().url(urlRequest).build();
-
-        try (Response response = httpClient.newCall(request).execute()) {
-            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-
-            String body = response.body().string();
-
-            System.out.println(body);
-        }
-
-    }
 }
---
+++ src/main/java/com/bassolicodes/utils/SendRequest.java
@@ -0,0 +1,54 @@
+package com.bassolicodes.utils;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.io.IOException;
+
+public class SendRequest {
+
+    public void getUserGitHub(String username) throws IOException {
+
+        if(username == null || username.isEmpty()) {
+            throw new IOException("Username is null or empty");
+        }
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://api.github.com/users/" + username;
+
+        Request request = new Request.Builder().url(url).build();
+
+        try (okhttp3.Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            String body = response.body().string();
+
+            System.out.println(body);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public void getAllContributionsByUser(String username) throws IOException {
+        if(username == null) {
+            throw new IOException("Username is null");
+        }
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://api.github.com/users/" + username + "/events/public";
+
+        Request request = new Request.Builder().url(url).build();
+
+        try (okhttp3.Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            String body = response.body().string();
+
+            System.out.println(body);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
